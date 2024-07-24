115319fdd2737a03d2e7597f22addea3b5ce0b4b
P D <gaimukhe.pooja.dev@gmail.com>
2024-07-24 20:32:38 +0530

    Initial commit

--- src/main/java/com/hackathon/skillhunt/SkillHuntApplication.java
+++ src/main/java/com/hackathon/skillhunt/SkillHuntApplication.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.hackathon.shillhunt")
 public class SkillHuntApplication {
 	private static final Logger log = LoggerFactory.getLogger(SkillHuntApplication.class);
 	public static void main(String[] args) {
